9a0b5e400070eaac3f9af7b8b60d537b4cf9019b
Kenton Newby <kentonnewby@gmail.com>
2012-04-14 19:14:13 -0400

    - got rought cut working - searches seed URL and then subsequent URLs on second call to #crawl   method

--- lib/search/crawler.rb
+++ lib/search/crawler.rb
@@ -7,10 +7,11 @@ class Crawler
 
   USER_AGENT = "foo-spider"
 
-  attr_accessor :urls, :robot
+  attr_accessor :url_file, :urls, :robot
 
   def initialize(url_file = "./lib/search/urls.yml")
-    @urls = YAML::load_file(File.open(url_file))
+    @url_file = url_file
+    @urls = YAML::load_file(File.open(@url_file))
     @robot = Robots.new USER_AGENT
   end
 
@@ -22,14 +23,42 @@ class Crawler
     return [] unless allowed_to_crawl?(requested_url)
 
     webpage = Net::HTTP.get(URI(requested_url))
-    page = Nokogiri::HTML(webpage)
-    all_anchor_tags = page.css('a')
-    all_links = all_anchor_tags.map { |tag| tag.attributes["href"].value}
+    parsed_page = Nokogiri::HTML(webpage)
+    all_anchor_tags = parsed_page.css('a')
+    all_links = all_anchor_tags.map { |tag| tag.attributes["href"].value unless tag.attributes["href"].nil? }
 
-    all_links.select do |link| 
-      link.gsub!(/^\//, requested_url + "/") if link.match(/^\/.*/)
+    all_links.select do |link|
+      link.gsub!(/^\//, requested_url + "/") if link.match(/^\/.*/) unless link.nil?
+      link.chop! if link.end_with?("/") unless link.nil?
     end
 
     all_links
   end
+
+  def save_newly_found_pages(new_pages)
+    File.open(url_file, "w") { |file| YAML.dump(new_pages, file) }
+  end
+
+  # seed
+  # - news.google.com => google.com, plus.google.com, reader.google.com
+
+  def crawl
+    new_pages = []
+
+      urls.each do |url|
+        add_to_index(url)
+        new_pages.concat(links_on_page(url))
+      end
+
+      self.urls = new_pages
+
+    save_newly_found_pages(new_pages)
+  end
+
+  def add_to_index(url)
+    File.open("./output.txt", "a+") do |file|
+      file.puts url
+    end
+  end
+
 end
--- spec/lib/search/crawler_spec.rb
+++ spec/lib/search/crawler_spec.rb
@@ -1,43 +1,52 @@
 require 'spec_helper'
 
 describe Crawler do
-  it "loads a list of seed URLs from a .yml file" do
-    url_file = File.open("./fixtures/urls.yml")
-    crawler = Crawler.new(url_file)
-    crawler.urls.should == ["http://news.google.com", "http://cnn.com", "http://loyola.edu"]
+  describe "initialization steps" do
+    it "saves the url_file parameter to an instance variable" do
+      url_file = "./fixtures/urls.yml"
+      crawler = Crawler.new(url_file)
+      crawler.url_file.should == "./fixtures/urls.yml"
+    end
+
+    it "loads a list of seed URLs from a .yml file" do
+      url_file = "./fixtures/urls.yml"
+      crawler = Crawler.new(url_file)
+      crawler.urls.should == ["http://news.google.com", "http://cnn.com", "http://loyola.edu"]
+    end
+
+    it "sets the USER_AGENT" do
+      robot = Robots.new Crawler::USER_AGENT
+      crawler = Crawler.new
+      crawler.robot.should be_a(Robots)
+    end
   end
 
-  it "sets the USER_AGENT" do
-    robot = Robots.new Crawler::USER_AGENT
-    crawler = Crawler.new
-    crawler.robot.should be_a(Robots)
-  end
-
-  describe "allowed_to_crawl?" do
-    it "returns true if user_agent is allowed to crawl the site" do
+  describe "#allowed_to_crawl?" do
+    it "returns true if user_agent is allowed to crawl the requested site" do
       page = "http://www.google.com"
       Robots.any_instance.stub(:allowed?).with(page).and_return(true)
       crawler = Crawler.new
       crawler.allowed_to_crawl?(page).should == true
     end
 
-    it "returns false if user_agent is not allowed to crawl the site" do
+    it "returns false if user_agent is not allowed to crawl the requested site" do
       page = "http://www.google.com"
       Robots.any_instance.stub(:allowed?).with(page).and_return(false)
       crawler = Crawler.new
       crawler.allowed_to_crawl?(page).should == false
     end
   end
-     # stub Nokogiri call here...
-     # get HTML page:
-     #   webpage = Net::HTTP.get(URI("http://kentonnewby.com"))
-     # parse a page:
-     #   page = Nokogiri::HTML(webpage)
-     # all links:
-     #   page.css('a')
-     # external links:
-     #   links.map { |l| l.attributes["href"].value }.select { |l| l.match(/^http:*/) }
-  describe "parse_url(url)", :vcr do
+
+  # stub Nokogiri call here...
+  # get HTML page:
+  #   webpage = Net::HTTP.get(URI("http://kentonnewby.com"))
+  # parse a page:
+  #   page = Nokogiri::HTML(webpage)
+  # all links:
+  #   page.css('a')
+  # external links:
+  #   links.map { |l| l.attributes["href"].value }.select { |l| l.match(/^http:*/) }
+  describe "#links_on_page", :vcr do
 
     before(:each) do
       @url = "http://kentonnewby.com"
@@ -51,61 +60,61 @@ describe Crawler do
     end
 
     it "parses the URL for a page that can be added to the index and returns a list of external links" do
-      array_of_urls = ["http://kentonnewby.com/", 
+      array_of_urls = ["http://kentonnewby.com", 
                        "http://kentonnewby.com/about", 
                        "http://kentonnewby.com/profile", 
                        "http://kentonnewby.com/bucket-list", 
                        "http://kentonnewby.com/contact", 
-                       "http://kentonnewby.com/", 
-                       "http://kentonnewby.com/", 
-                       "http://kentonnewby.com/blog/categories/web-development/", 
-                       "http://kentonnewby.com/blog/categories/ruby/",
-                       "http://kentonnewby.com/blog/categories/rails/",
-                       "http://kentonnewby.com/blog/categories/jquery/",
-                       "http://kentonnewby.com/blog/categories/backbonejs/",
-                       "http://kentonnewby.com/blog/categories/nodejs/",
-                       "http://kentonnewby.com/blog/categories/mobile-development/",
-                       "http://kentonnewby.com/blog/categories/ios/",
-                       "http://kentonnewby.com/blog/categories/mobile-web/",
-                       "http://kentonnewby.com/blog/categories/business/",
-                       "http://kentonnewby.com/blog/categories/marketing/",
-                       "http://kentonnewby.com/blog/categories/sales/",
-                       "http://kentonnewby.com/blog/categories/lifestyle-design/",
-                       "http://kentonnewby.com/blog/categories/inner-game/",
-                       "http://kentonnewby.com/blog/categories/productivity/",
-                       "http://kentonnewby.com/blog/categories/inspirational/",
-                       "http://kentonnewby.com/blog/categories/books/",
+                       "http://kentonnewby.com", 
+                       "http://kentonnewby.com", 
+                       "http://kentonnewby.com/blog/categories/web-development", 
+                       "http://kentonnewby.com/blog/categories/ruby",
+                       "http://kentonnewby.com/blog/categories/rails",
+                       "http://kentonnewby.com/blog/categories/jquery",
+                       "http://kentonnewby.com/blog/categories/backbonejs",
+                       "http://kentonnewby.com/blog/categories/nodejs",
+                       "http://kentonnewby.com/blog/categories/mobile-development",
+                       "http://kentonnewby.com/blog/categories/ios",
+                       "http://kentonnewby.com/blog/categories/mobile-web",
+                       "http://kentonnewby.com/blog/categories/business",
+                       "http://kentonnewby.com/blog/categories/marketing",
+                       "http://kentonnewby.com/blog/categories/sales",
+                       "http://kentonnewby.com/blog/categories/lifestyle-design",
+                       "http://kentonnewby.com/blog/categories/inner-game",
+                       "http://kentonnewby.com/blog/categories/productivity",
+                       "http://kentonnewby.com/blog/categories/inspirational",
+                       "http://kentonnewby.com/blog/categories/books",
                        "http://feeds.feedburner.com/kentonnewby",
                        "http://twitter.com/kentonnewby",
                        "http://kentonnewby.com/quick-git-summaries-using-git-shortlog",
-                       "http://kentonnewby.com/blog/categories/git/",
+                       "http://kentonnewby.com/blog/categories/git",
                        "http://kentonnewby.com/quick-git-summaries-using-git-shortlog",
                        "http://kentonnewby.com/benefits-of-tdd",
-                       "http://kentonnewby.com/blog/categories/rails/",
-                       "http://kentonnewby.com/blog/categories/ruby/",
+                       "http://kentonnewby.com/blog/categories/rails",
+                       "http://kentonnewby.com/blog/categories/ruby",
                        "http://kentonnewby.com/benefits-of-tdd",
                        "http://kentonnewby.com/are-you-a-producer-or-a-consumer",
-                       "http://kentonnewby.com/blog/categories/inner-game/",
+                       "http://kentonnewby.com/blog/categories/inner-game",
                        "http://kentonnewby.com/are-you-a-producer-or-a-consumer",
                        "http://kentonnewby.com/amending-last-git-commit",
-                       "http://kentonnewby.com/blog/categories/git/",
+                       "http://kentonnewby.com/blog/categories/git",
                        "http://kentonnewby.com/amending-last-git-commit",
                        "http://kentonnewby.com/ios-development-workflow",
-                       "http://kentonnewby.com/blog/categories/ios/",
-                       "http://kentonnewby.com/blog/categories/mobile-development/",
+                       "http://kentonnewby.com/blog/categories/ios",
+                       "http://kentonnewby.com/blog/categories/mobile-development",
                        "http://kentonnewby.com/ios-development-workflow",
-                       "http://kentonnewby.com/page/2/",
+                       "http://kentonnewby.com/page/2",
                        "http://kentonnewby.com/blog/archives",
                        "http://kentonnewby.com/about",
                        "http://codecanyon.net?ref=kentonnewby",
                        "http://themeforest.net?ref=kentonnewby",
-                       "http://f52f6a2culudsoi0rj6a0h3rbu.hop.clickbank.net/",
+                       "http://f52f6a2culudsoi0rj6a0h3rbu.hop.clickbank.net",
                        "http://www.mailchimp.com",
                        "http://www.railscasts.com",
                        "http://www.peepcode.com",
                        "http://www.destroyallsoftware.com/screencasts",
                        "http://www.practicingruby.com",
-                       "http://www.startupsfortherestofus.com/",
+                       "http://www.startupsfortherestofus.com",
                        "http://kentonnewby.com/privacy",
                        "http://kentonnewby.com/earnings-disclaimer",
                        "http://kentonnewby.com/terms-of-use",
@@ -121,13 +130,33 @@ describe Crawler do
     end
   end
 
+  describe "#save_newly_found_pages" do
+    it "writes the list of newly found pages to the file that stores the list of URLs" do
+      url_file = "./fixtures/urls.yml"
+      new_url_list = url_file = "./fixtures/new_url_list.yml"
+      new_pages = ["http://kentonnewby.com", "http://rubyonrails.org", "http://mailchimp.com"]
+      crawler = Crawler.new(url_file)
+      crawler.stub(:links_on_page).and_return(new_pages)
+
+      crawler.save_newly_found_pages(new_pages)
+      YAML.load_file(url_file).should == new_pages
+    end
+  end
+
+  describe "#crawl" do
+    it "crawls the list of URLs looking for new links if the URLs allow crawler to index the site" do
+      
+    end
+  end
+
   describe "it processes the pages in the seed URL list and any newly found pages until no more pages can be found" do
     #it "determines if a page can be added to the index"
     #it "retrieves each page that can be added to the index"
-    it "parses the URLs on each page and saves them to a collection"
+    #it "parses the URLs on each page and saves them to a collection"
     it "prints a general error message if an exception is raised"
     it "prints a timeout error message if a TimeoutException is raised"
-    it "writes the URLs for the newly found pages to the end of the .yml file"
-
+    #it "writes the URLs for the newly found pages to the end of the .yml file"
+    # File.open(URL_LIST, "w") { |file| YAML.dump(stuff, file) }
   end
+
 end
