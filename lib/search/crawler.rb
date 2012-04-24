47887b1c903eff9228d5978a19697f7f295aa462
Kenton Newby <kentonnewby@gmail.com>
2012-04-23 22:40:24 -0400

    - refactored stuff out of Crawler that didn't belong there - updated specs - still have one failing spec that needs to be fixed

--- lib/search/crawler.rb
+++ lib/search/crawler.rb
@@ -3,11 +3,6 @@ require 'robots'
 require 'net/http'
 require 'nokogiri'
 
-
-Mongoid.configure do |config|
-  config.master = Mongo::Connection.new.db("searchengine")
-end
-
 class Crawler
 
   USER_AGENT = "foo-spider"
@@ -24,22 +19,6 @@ class Crawler
     robot.allowed?(page)
   end
 
-  def links_on_page(requested_url)
-    return [] unless allowed_to_crawl?(requested_url)
-
-    webpage = Net::HTTP.get(URI(requested_url))
-    parsed_page = Nokogiri::HTML(webpage)
-    all_anchor_tags = parsed_page.css('a')
-    all_links = all_anchor_tags.map { |tag| tag.attributes["href"].value unless tag.attributes["href"].nil? }
-
-    all_links.select do |link|
-      link.gsub!(/^\//, requested_url + "/") if link.match(/^\/.*/) unless link.nil?
-      link.chop! if link.end_with?("/") unless link.nil?
-    end
-
-    all_links
-  end
-
   def save_newly_found_pages(new_pages)
     File.open(url_file, "w") { |file| YAML.dump(new_pages, file) }
   end
@@ -52,8 +31,16 @@ class Crawler
 
     urls.each do |url|
       begin
-        add_to_index(url)
-        new_pages.concat(links_on_page(url))
+        page = Page.find_or_initialize_by(:url => url)
+        if page.new_record?
+          page.add
+        elsif page.stale?
+          page.update
+        else
+          page.ignore
+        end
+
+        new_pages.concat(page.links)
       rescue
         next
       end
@@ -61,30 +48,9 @@ class Crawler
 
     self.urls = new_pages.uniq
 
-    save_newly_found_pages(new_pages)
+    save_newly_found_pages(new_pages.uniq)
 
     new_pages.uniq
-  end
-
-  def add_to_index(url)
-    page = Page.find_or_initialize_by(:url => url)
-    if page.new_record?
-      # save new record to database
-      puts "Adding the following URL to the index: #{url}"
-      page.update_attributes!(:title      => page.title,
-                              :keywords   => page.keywords,
-                              :created_at => Time.now,
-                              :updated_at => Time.now)
-    else
-      # update existing record in database
-      puts "Updating the following URL in the index: #{url}"
-      page.keywords = nil
-      page.title    = nil
-      page.update_attributes!(:title      => page.title,
-                              :keywords   => page.keywords,
-                              :updated_at => Time.now)
-
-    end
   end
 
 end
--- spec/lib/search/crawler_spec.rb
+++ spec/lib/search/crawler_spec.rb
@@ -46,89 +46,6 @@ describe Crawler do
   #   page.css('a')
   # external links:
   #   links.map { |l| l.attributes["href"].value }.select { |l| l.match(/^http:*/) }
-  describe "#links_on_page", :vcr do
-
-    before(:each) do
-      @url = "http://kentonnewby.com"
-      @crawler = Crawler.new
-    end
-
-    it "returns empty unless requested URL allows crawler to crawl page" do
-      @crawler.stub(:allowed_to_crawl?).and_return(false)
-
-      @crawler.links_on_page(@url).should == []
-    end
-
-    it "parses the URL for a page that can be added to the index and returns a list of external links" do
-      array_of_urls = ["http://kentonnewby.com",
-                       "http://kentonnewby.com/about",
-                       "http://kentonnewby.com/profile",
-                       "http://kentonnewby.com/bucket-list",
-                       "http://kentonnewby.com/contact",
-                       "http://kentonnewby.com",
-                       "http://kentonnewby.com",
-                       "http://kentonnewby.com/blog/categories/web-development",
-                       "http://kentonnewby.com/blog/categories/ruby",
-                       "http://kentonnewby.com/blog/categories/rails",
-                       "http://kentonnewby.com/blog/categories/jquery",
-                       "http://kentonnewby.com/blog/categories/backbonejs",
-                       "http://kentonnewby.com/blog/categories/nodejs",
-                       "http://kentonnewby.com/blog/categories/mobile-development",
-                       "http://kentonnewby.com/blog/categories/ios",
-                       "http://kentonnewby.com/blog/categories/mobile-web",
-                       "http://kentonnewby.com/blog/categories/business",
-                       "http://kentonnewby.com/blog/categories/marketing",
-                       "http://kentonnewby.com/blog/categories/sales",
-                       "http://kentonnewby.com/blog/categories/lifestyle-design",
-                       "http://kentonnewby.com/blog/categories/inner-game",
-                       "http://kentonnewby.com/blog/categories/productivity",
-                       "http://kentonnewby.com/blog/categories/inspirational",
-                       "http://kentonnewby.com/blog/categories/books",
-                       "http://feeds.feedburner.com/kentonnewby",
-                       "http://twitter.com/kentonnewby",
-                       "http://kentonnewby.com/quick-git-summaries-using-git-shortlog",
-                       "http://kentonnewby.com/blog/categories/git",
-                       "http://kentonnewby.com/quick-git-summaries-using-git-shortlog",
-                       "http://kentonnewby.com/benefits-of-tdd",
-                       "http://kentonnewby.com/blog/categories/rails",
-                       "http://kentonnewby.com/blog/categories/ruby",
-                       "http://kentonnewby.com/benefits-of-tdd",
-                       "http://kentonnewby.com/are-you-a-producer-or-a-consumer",
-                       "http://kentonnewby.com/blog/categories/inner-game",
-                       "http://kentonnewby.com/are-you-a-producer-or-a-consumer",
-                       "http://kentonnewby.com/amending-last-git-commit",
-                       "http://kentonnewby.com/blog/categories/git",
-                       "http://kentonnewby.com/amending-last-git-commit",
-                       "http://kentonnewby.com/ios-development-workflow",
-                       "http://kentonnewby.com/blog/categories/ios",
-                       "http://kentonnewby.com/blog/categories/mobile-development",
-                       "http://kentonnewby.com/ios-development-workflow",
-                       "http://kentonnewby.com/page/2",
-                       "http://kentonnewby.com/blog/archives",
-                       "http://kentonnewby.com/about",
-                       "http://codecanyon.net?ref=kentonnewby",
-                       "http://themeforest.net?ref=kentonnewby",
-                       "http://f52f6a2culudsoi0rj6a0h3rbu.hop.clickbank.net",
-                       "http://www.mailchimp.com",
-                       "http://www.railscasts.com",
-                       "http://www.peepcode.com",
-                       "http://www.destroyallsoftware.com/screencasts",
-                       "http://www.practicingruby.com",
-                       "http://www.startupsfortherestofus.com",
-                       "http://kentonnewby.com/privacy",
-                       "http://kentonnewby.com/earnings-disclaimer",
-                       "http://kentonnewby.com/terms-of-use",
-                       "http://kentonnewby.com/sitemap.xml",
-                       "http://octopress.org"
-                      ]
-
-      @crawler.links_on_page(@url).should == array_of_urls
-    end
-
-    it "adds hostname to relative links relative links" do
-      @crawler.links_on_page(@url).should include("http://kentonnewby.com/about")
-    end
-  end
 
   describe "#save_newly_found_pages" do
     it "writes the list of newly found pages to the file that stores the list of URLs" do
@@ -136,7 +53,7 @@ describe Crawler do
       new_url_list = url_file = "./fixtures/new_url_list.yml"
       new_pages = ["http://kentonnewby.com", "http://rubyonrails.org", "http://mailchimp.com"]
       crawler = Crawler.new(url_file)
-      crawler.stub(:links_on_page).and_return(new_pages)
+      Page.any_instance.stub(:links).and_return(new_pages)
 
       crawler.save_newly_found_pages(new_pages)
       YAML.load_file(url_file).should == new_pages
@@ -148,69 +65,10 @@ describe Crawler do
       crawler = Crawler.new
       crawler.urls = ["http://kentonnewby.com"]
       newly_found_pages = ["http://kentonnewby.com/about", "http://kentonnewby.com", "http://kentonnewby.com/contact", "http://twitter.com/kentonnewby"]
-      crawler.stub(:links_on_page).and_return(newly_found_pages)
+      Page.any_instance.stub(:links).and_return(newly_found_pages)
 
       crawler.crawl.should == newly_found_pages
     end
   end
-
-  # tries to find a page in the database that matches that url
-  # does a find_or_create_by_url
-  # if the page is a new_record?
-  #    #... index it
-  # else if the page is stale,
-  #   #...update it
-  # else
-  #     #...do nothing
-  #
-  # Word.where("pages.url" => "http://kentonnewby.com")
-  describe "#add_to_index" do
-    it "adds new pages to the index" do
-      crawler = Crawler.new
-      new_page = Page.new(:url => 'http://somesite.com')
-      new_page.stub(:new_record?).and_return(true)
-      Page.should_receive(:find_or_create_by_url).and_return(new_page)
-      url = "http://somesite.com"
-
-      lambda { crawler.add_to_index(url) }.should change(Page, :count).by(1)
-    end
-    it "updates pages that are already in the index"
-  end
-
-  describe "#index_page"
-    # get all words on the page
-    # ********  need to see if word is new or already in the dB
-    # words.each do |word|
-    #   w = Word.new(:stem => word)
-    #   new_page = Page.new(:url => self.url,
-    #                       :title => self.title
-    #                       :created_at => DateTime.now
-    #                       :updated_at => DateTime.now)
-    #   new_location = Location.new(:position => the word's index)
-    #   new_page.locations.push(new_location)
-    #   w.pages.push(new_page)
-    #
-    # end
-
-  describe "#update_page"
-
-
-
-
-
-
-
-  #describe "it processes the pages in the seed URL list and any newly found pages until no more pages can be found" do
-    #it "determines if a page can be added to the index"
-    #it "retrieves each page that can be added to the index"
-    #it "parses the URLs on each page and saves them to a collection"
-    it "prints a general error message if an exception is raised"
-    it "prints a timeout error message if a TimeoutException is raised"
-    #it "writes the URLs for the newly found pages to the end of the .yml file"
-    # File.open(URL_LIST, "w") { |file| YAML.dump(stuff, file) }
-  #end
-
-
-
 
 end
